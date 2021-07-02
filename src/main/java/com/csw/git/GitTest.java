15af6944ebc4090db4779ca8526795eb2a2abc36
todcsw <1759864302@qq.com>
2021-07-02 18:03:00 +0800

    first commit

---
+++ src/main/java/com/csw/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.csw.git;
+
+/**
+ * @Auther: 行路
+ * @Email: 1759864302@qq.com
+ * @Date: Created on 2021/7/2 17:59 星期五
+ * @Description:
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
